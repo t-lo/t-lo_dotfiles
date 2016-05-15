37c91eeaf87b763e8b3404da297ff27232f7793e
Thilo Fromm <github@thilo-fromm.de>
2016-05-15 09:37:26 +0200

    installer now removes itself before initial check-out
    Signed-off-by: Thilo Fromm <github@thilo-fromm.de>

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-dotfiles_root=$HOME
+script_name="$0"
+dotfiles_root=$HOME/tst
+
+rm "$script_name"
 git init $dotfiles_root
 git remote add origin git://github.com/t-lo/t-lo_dotfiles.git
 git fetch
